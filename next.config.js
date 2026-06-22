83cd59111ccc2204cb3c0c5de04475de23aa655b
Seenwin01 <punseen@gmail.com>
2026-06-22 16:42:41 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     // allow large video uploads through API routes
     serverActions: { bodySizeLimit: "200mb" },
+  },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
   },
 };
 
